9d84da6ac5cde2e3553aefb70aa1e1ca5efec1b5
daniele-ng <daniele.negretti2@gmail.com>
2025-10-05 21:30:56 +0200

    created new component Select for client form

---
+++ client/src/components/form/SelectComponent.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+defineProps<{
+    id: string,
+    name: string,
+    options: { [key: string]: string }[]
+    selectedOption?: string
+    required?: boolean
+}>()
+
+const style: string = "w-full border h-9 px-2 box-border border-gray-200 focus:border-blue-300 focus:ring-0 focus:outline-none rounded-md shadow-sm"
+
+</script>
+
+<template>
+    <select :id="id" :name="name" :class="style" :required="required">
+        <option v-for="value in options" v-bind:key="value.key" :value="value.key" :selected="selectedOption == value.key">{{ value.label }}</option>
+    </select>
+</template>
