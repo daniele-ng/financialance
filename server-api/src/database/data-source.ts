dd1ec07e72f9b56f975c35a6a4ea55c8a3f88720
daniele-ng <daniele.negretti2@gmail.com>
2025-10-05 21:35:58 +0200

    update server version to 1.0.0

--- server-api/src/database/data-source.ts
+++ server-api/src/database/data-source.ts
@@ -6,7 +6,7 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 export const AppDataSourceOptions: DataSourceOptions = {        
     type: "sqlite",
-    database: process.env.DATABASE_NAME ? "./src/database/" +  process.env.DATABASE_NAME : "./src/database/database.db",
+    database: process.env.DATABASE_NAME ? "dist/database/" +  process.env.DATABASE_NAME : "dist/database/database.db",
     synchronize: process.env.DATABASE_SYNCHRONIZE,
     entities: ["dist/entities/*.entity.js"],        
     migrationsRun: process.env.DATABASE_MIGRATION_AUTO_RUN,
