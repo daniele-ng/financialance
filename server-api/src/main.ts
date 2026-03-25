a938c267ee5bdeda9b55b40708cf5626add16740
daniele-ng <daniele.negretti2@gmail.com>
2026-03-25 18:49:00 +0100

    server - set up cors policy

--- server-api/src/main.ts
+++ server-api/src/main.ts
@@ -4,9 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { validationExceptionFactory } from './lib/mocks/validation-exception-factory';
 import { useContainer } from 'class-validator';
 import { ValidatorsModule } from './validators/validators.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
+
+    const corsOptions = {
+        "origin": process.env.CORS_ALLOWED_ORIGIN,
+        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+        "credentials": true,
+        "preflightContinue": false,
+        "optionsSuccessStatus": 204
+    }
+
+    const app = await NestFactory.create(AppModule, { cors: corsOptions });
 
     app.useGlobalPipes(
         new ValidationPipe(
@@ -19,6 +29,8 @@ async function bootstrap() {
     )
 
     useContainer(app.select(ValidatorsModule), { fallbackOnErrors: true })
+
+    app.use(cookieParser());
 
     await app.listen(process.env.PORT ?? 3000);
 }
